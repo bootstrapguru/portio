431ac88e7754506cd6d2e6917d99eee6dcd809f0
vijay kumar <hire@vijaykumar.me>
2013-02-20 07:53:23 +0530

    one page design some look

--- js/scroll.js
+++ js/scroll.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         var targetOffset = $target.offset().top;
         $(this).click(function(event) {
           event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+          $(scrollElem).animate({scrollTop: targetOffset}, 1000, function() {
             location.hash = target;
           });
         });
